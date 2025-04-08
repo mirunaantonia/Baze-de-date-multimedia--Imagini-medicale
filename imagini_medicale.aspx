bf89816e88eb4ec0a46724a2832db61643c8bd97
Miruna Vasile <miruna202@yahoo.com>
2025-04-08 13:03:13 +0300

    Add files via upload

---
+++ imagini_medicale.aspx
@@ -0,0 +1,102 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="imagini_medicale.aspx.cs" Inherits="proiect_imagini_medicale_vasilemiruna.imagini_medicale" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Label ID="Label1" runat="server" BackColor="#FF99CC" BorderColor="#FF0066" BorderStyle="Inset" ForeColor="White" Text="Gestionarea imaginilor medicale" Font-Bold="True" Font-Size="XX-Large"></asp:Label>
+        <p>
+            <asp:Label ID="Ecou" runat="server" ForeColor="#FF6699" Text="Zona destinata erorilor "></asp:Label>
+        </p>
+        <asp:Label ID="Label2" runat="server" BackColor="#FF6699" Text="Incarcare imagini medicale" Font-Bold="True" Font-Italic="True" Font-Size="X-Large"></asp:Label>
+        <p>
+            <asp:Label ID="Label3" runat="server" Text="Cod imagine :"></asp:Label>
+            <asp:TextBox ID="tb_cod" runat="server" BackColor="#FF99CC" Width="157px"></asp:TextBox>
+        </p>
+        <asp:Label ID="Label4" runat="server" Text="Descrierea imaginii :"></asp:Label>
+        <asp:TextBox ID="tb_descriere" runat="server" BackColor="#FF99CC" Height="48px" Width="935px"></asp:TextBox>
+        <p>
+            <asp:Label ID="Label5" runat="server" Text="Insereaza imaginea: "></asp:Label>
+            <asp:FileUpload ID="incarcareimagine" runat="server" BackColor="#FF99CC" Height="45px" Width="429px" />
+        </p>
+        <asp:Label ID="Label6" runat="server" Text="Data incarcare :"></asp:Label>
+        <asp:TextBox ID="tb_dataincarcare" runat="server" BackColor="#FF99CC" Height="31px" Width="206px"></asp:TextBox>
+        <asp:Label ID="Label7" runat="server" Text="(introduceti o data in formatul DD-MM-YYYY)"></asp:Label>
+        <p>
+            <asp:Label ID="Label8" runat="server" Text="Rezultate analiza : "></asp:Label>
+            <asp:TextBox ID="tb_rezultateanaliza" runat="server" BackColor="#FF99CC" Height="42px" Width="954px"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="IncarcaImagineaInSistem" runat="server" BackColor="Black" BorderColor="Black" BorderStyle="Outset" ForeColor="White" Height="53px" OnClick="IncarcaImagineaInSistem_Click" Text="Incarca imaginea in sistem" Width="644px" Font-Bold="True" Font-Size="Large" />
+        </p>
+        <p>
+            <asp:Label ID="Label9" runat="server" BackColor="#FF6699" Font-Bold="True" Font-Italic="True" Font-Size="X-Large" Text="Vizualizare imagini medicale incarcate in sistem"></asp:Label>
+        </p>
+        <asp:Label ID="Label10" runat="server" Text="Cod imagine: "></asp:Label>
+        <asp:TextBox ID="codafisareimagine" runat="server" BackColor="#FF99CC"></asp:TextBox>
+        <p>
+            <asp:Button ID="afisareimagine" runat="server" BackColor="Black" Font-Bold="True" Font-Size="Large" ForeColor="White" Height="79px" OnClick="afisareimagine_Click" Text="Afisare Imagine" Width="272px" />
+            <asp:Image ID="imagineaafisata" runat="server" BackColor="#FF99CC" Height="260px" Width="437px" />
+        </p>
+        <p>
+            <asp:TextBox ID="afisaredescriere" runat="server" BackColor="#FF99CC" Width="824px"></asp:TextBox>
+        </p>
+        <asp:TextBox ID="afisaredataincarcare" runat="server" BackColor="#FF99CC" Width="824px"></asp:TextBox>
+        <p>
+            <asp:TextBox ID="afisarerezultate" runat="server" BackColor="#FF99CC" Width="822px"></asp:TextBox>
+        </p>
+        <asp:Label ID="Label11" runat="server" BackColor="#993366" Font-Bold="True" Font-Size="X-Large" Text="SCINTIGRAFIE (Imagini de medicina nucleara)"></asp:Label>
+        <p>
+            <asp:Label ID="Ecouu" runat="server" BackColor="#660033" ForeColor="White" Text="Sectiunea de erori"></asp:Label>
+        </p>
+        <asp:Label ID="substantaradioactiva" runat="server" BackColor="#993366" Text="Substanta radioactiva"></asp:Label>
+        <asp:CheckBox ID="technetiu" runat="server" Text="Technetiu      " />
+        <asp:CheckBox ID="iod" runat="server" Text="IOD           " />
+        <asp:CheckBox ID="Cfluor" runat="server" Text="Fluor" />
+        <p>
+            <asp:Label ID="numepacient" runat="server" BackColor="#993366" Text="Nume pacient   :"></asp:Label>
+            <asp:TextBox ID="numepacientintrodus" runat="server"></asp:TextBox>
+            <asp:Label ID="Label12" runat="server" BackColor="#660033" Text="            ID:"></asp:Label>
+            <asp:TextBox ID="idnuclear" runat="server"></asp:TextBox>
+        </p>
+        <asp:FileUpload ID="imaginenucleara" runat="server" />
+        <br />
+        <asp:Button ID="incarcanucleara" runat="server" OnClick="incarcanucleara_Click" Text="Incarca imaginea de medicina nucleara" />
+        <p>
+            <asp:Button ID="genereazasemnatura" runat="server" Height="57px" OnClick="genereazasemnatura_Click" Text="Genereaza semnatura" Width="812px" />
+        </p>
+        <asp:Label ID="Label13" runat="server" BackColor="#660033" ForeColor="White" Text="Coeficientul de importanta al culorii"></asp:Label>
+        <asp:TextBox ID="culoare" runat="server"></asp:TextBox>
+        <p>
+            <asp:Label ID="Label14" runat="server" BackColor="#660033" ForeColor="White" Text="Coeficientul de textura: "></asp:Label>
+            <asp:TextBox ID="textura" runat="server"></asp:TextBox>
+        </p>
+        <asp:Label ID="Label15" runat="server" BackColor="#660033" ForeColor="White" Text="Coeficientul forma: "></asp:Label>
+        <asp:TextBox ID="forma" runat="server"></asp:TextBox>
+        <p>
+            <asp:Label ID="Label16" runat="server" BackColor="#660033" ForeColor="White" Text="Coeficientul locatie :"></asp:Label>
+            <asp:TextBox ID="locatie" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="imaginearezultat" runat="server" BackColor="#FF6699" Text="ID-ul imaginii rezultat: "></asp:Label>
+            <asp:TextBox ID="idrezultat" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="regasireimagine" runat="server" BackColor="#CC6699" Height="83px" OnClick="regasireimagine_Click" Text="Buton regasire imagine" Width="339px" />
+            <asp:Image ID="imagineaa" runat="server" BackColor="#660033" Height="201px" Width="306px" />
+            <asp:Button ID="butonafisid" runat="server" BackColor="#CC6699" OnClick="butonafisid_Click" style="margin-top: 0px" Text="Buton afisare imagine cu ID-ul rezultat" Width="513px" />
+        </p>
+        <br />
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ imagini_medicale.aspx.cs
@@ -0,0 +1,362 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.EnterpriseServices;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
+
+
+namespace proiect_imagini_medicale_vasilemiruna
+{
+    public partial class imagini_medicale : System.Web.UI.Page
+    {
+        private OracleConnection con;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string cons = "User ID=STUD_VASILEM; Password=student; Data Source=(DESCRIPTION=" +
+                          "(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=37.120.249.41)(PORT=1521)))" +
+                          "(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=orcls)));";
+            con = new OracleConnection(cons);
+        }
+
+        protected void IncarcaImagineaInSistem_Click(object sender, EventArgs e)
+        {
+            Ecou.Text = "";
+            if (incarcareimagine.HasFile)
+            {
+                try
+                {
+                    string extension = Path.GetExtension(incarcareimagine.FileName).ToLower();
+                    if (extension != ".jpg" && extension != ".png" && extension != ".gif" && extension != ".dicom" && extension != ".dcm")
+                    {
+                        Ecou.Text = "Tipul de fisier nu este acceptat. Te rugam sa incarci o imagine medicala valida (.jpg, .png, .gif, .dicom, .dcm).";
+                        return;
+                    }
+
+                    string filePath = Path.Combine(@"C:\Users\Miruu\Desktop\proiect baze multi\imagini medicale\" + incarcareimagine.FileName);
+                    incarcareimagine.SaveAs(filePath);
+                    Ecou.Text = "Fisierul incarcat este " + incarcareimagine.FileName;
+
+                    byte[] imageBytes = File.ReadAllBytes(filePath);
+                    Ecou.Text += " Imaginea medicala incarcata are dimensiunea de : " + imageBytes.Length + " bytes.";
+                    DateTime dataIncarcare = DateTime.Now;
+                    con.Open();
+                    OracleCommand cmd = new OracleCommand("ins_imagine_medicala", con)
+                    {
+                        CommandType = CommandType.StoredProcedure
+                    };
+                    cmd.Parameters.Add("vcod", OracleDbType.Int32).Value = Convert.ToInt32(tb_cod.Text);
+                    cmd.Parameters.Add("vdescriere", OracleDbType.Varchar2).Value = tb_descriere.Text;
+                    cmd.Parameters.Add("vfis", OracleDbType.Blob).Value = imageBytes;
+                    cmd.Parameters.Add("vdata_incarcare", OracleDbType.Date).Value = dataIncarcare;
+                    cmd.Parameters.Add("vrezultate_analiza", OracleDbType.Varchar2).Value = tb_rezultateanaliza.Text;
+
+                    if (con.State != ConnectionState.Open)
+                    {
+                        con.Open();
+                    }
+                    cmd.ExecuteNonQuery();
+                    Ecou.Text += "Imaginea a fost incarcata cu succes.";
+                }
+
+
+                catch (OracleException ex)
+                {
+                    Ecou.Text += " A aparut o eroare in timpul incarcarii imaginii medicale.  " + ex.Message;
+                }
+                catch (Exception ex)
+                {
+                    Ecou.Text += " Eroare generala.Te rugam să selectezi un fisier pentru incarcare.  " + ex.Message;
+                }
+                finally
+                {
+                    if (con.State == ConnectionState.Open)
+                    {
+                        con.Close();
+                    }
+                }
+            }
+            else
+            {
+                Ecou.Text = "Fișier imagine lipsă!";
+            }
+        }
+
+        protected void afisareimagine_Click(object sender, EventArgs e)
+        {
+            imagineaafisata.ImageUrl = "";
+            Ecou.Text = "";
+            afisaredescriere.Text = "";
+            afisaredataincarcare.Text = "";
+            afisarerezultate.Text = "";
+
+
+            try
+            {
+                con.Open();
+                OracleCommand cmd = new OracleCommand("selectare_afisimagine_medicala", con)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+
+             
+                cmd.Parameters.Add("vcod", OracleDbType.Int32).Value = Convert.ToInt32(codafisareimagine.Text);
+
+              
+                cmd.Parameters.Add("flux", OracleDbType.Blob).Direction = ParameterDirection.Output;
+                cmd.Parameters.Add("vdescriere", OracleDbType.Varchar2, 255).Direction = ParameterDirection.Output;
+                cmd.Parameters.Add("vdata_incarcare", OracleDbType.Date).Direction = ParameterDirection.Output;
+                cmd.Parameters.Add("vrezultate_analiza", OracleDbType.Varchar2, 4000).Direction = ParameterDirection.Output;
+
+                cmd.ExecuteNonQuery();
+
+    
+                if (cmd.Parameters["flux"].Value != DBNull.Value)
+                {
+
+                    OracleBlob blob = (OracleBlob)cmd.Parameters["flux"].Value;
+                    byte[] imageBytes = new byte[blob.Length];
+                    blob.Read(imageBytes, 0, imageBytes.Length);
+
+
+                    string base64Image = Convert.ToBase64String(imageBytes);
+                    imagineaafisata.ImageUrl = "data:image/gif;base64," + base64Image;
+
+                 
+                    afisaredescriere.Text = cmd.Parameters["vdescriere"].Value.ToString();
+                    DateTime dataIncarcare = ((OracleDate)cmd.Parameters["vdata_incarcare"].Value).Value;
+                    afisaredataincarcare.Text = dataIncarcare.ToString("dd-MM-yyyy");
+
+                    afisarerezultate.Text = cmd.Parameters["vrezultate_analiza"].Value.ToString();
+
+                    Ecou.Text = "Imaginea si detaliile au fost afisate cu succes.";
+                }
+                else
+                {
+                    Ecou.Text = "Nu s-a gasit nicio imagine pentru codul specificat.";
+                }
+            }
+            catch (OracleException ex)
+            {
+                Ecou.Text = "Eroare Oracle: " + ex.Message;
+            }
+            catch (Exception ex)
+            {
+                Ecou.Text = "Eroare generala: " + ex.Message;
+            }
+            finally
+            {
+                if (con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+            }
+
+
+        }
+
+        protected void incarcanucleara_Click(object sender, EventArgs e)
+        {
+            Ecouu.Text = "";
+
+            if (imaginenucleara.HasFile)
+            {
+                try
+                {
+                    string extension = Path.GetExtension(imaginenucleara.FileName).ToLower();
+                    if (extension != ".jpg" && extension != ".png" && extension != ".gif" && extension != ".dicom" && extension != ".dcm" && extension != ".jpeg")
+                    {
+                        Ecouu.Text = "Tipul de fișier nu este acceptat. Te rugăm să încarci o imagine medicală validă (.jpg, .png, .gif, .dicom, .dcm).";
+                        return;
+                    }
+
+                    string filePath = Path.Combine(@"C:\Users\Miruu\Desktop\proiect baze multi\imagini nucleare\", imaginenucleara.FileName);
+                    imaginenucleara.SaveAs(filePath);
+                    Ecouu.Text = "Fișierul încărcat este " + imaginenucleara.FileName;
+
+                    byte[] imageBytes = File.ReadAllBytes(filePath);
+                    Ecouu.Text += " Imaginea medicală încărcată are dimensiunea de : " + imageBytes.Length + " bytes.";
+
+                    con.Open();
+
+
+                    int idImagine = Convert.ToInt32(idnuclear.Text);
+
+                    OracleCommand cmd = new OracleCommand("ins_imagine_scintigrafie", con)
+                    {
+                        CommandType = CommandType.StoredProcedure
+                    };
+
+                    // Determinarea substanței selectate
+                    string substantaRadioactiva = "";
+                    if (technetiu.Checked) substantaRadioactiva = "Technetiu";
+                    else if (iod.Checked) substantaRadioactiva = "Iod";
+                    else if (Cfluor.Checked) substantaRadioactiva = "Fluor";
+
+                    cmd.Parameters.Add("v_id", OracleDbType.Int32).Value = Convert.ToInt32(idnuclear.Text);
+                    cmd.Parameters.Add("v_nume_pacient", OracleDbType.Varchar2).Value = numepacientintrodus.Text;
+                    cmd.Parameters.Add("v_substanta_radioactiva", OracleDbType.Varchar2).Value = substantaRadioactiva;
+                    cmd.Parameters.Add("v_imagine", OracleDbType.Blob).Value = imageBytes;
+
+                    cmd.ExecuteNonQuery();
+                    Ecouu.Text += "Imaginea scintigrafie a fost incarcata cu succes.";
+                }
+                catch (OracleException ex)
+                {
+                    Ecouu.Text += " A aparut o eroare in timpul incarcarii imaginii medicale. " + ex.Message;
+                }
+                catch (Exception ex)
+                {
+                    Ecouu.Text += " Eroare generala. Te rugam sa selectezi un fisier pentru incarcare. " + ex.Message;
+                }
+                finally
+                {
+                    if (con.State == ConnectionState.Open)
+                    {
+                        con.Close();
+                    }
+                }
+            }
+            else
+            {
+                Ecouu.Text = "Fisier imagine lipsa!";
+            }
+        }
+
+        protected void genereazasemnatura_Click(object sender, EventArgs e)
+        {
+            Ecouu.Text = "";
+            try
+            {
+                con.Open();
+            }
+            catch (OracleException ex)
+            {
+                Ecouu.Text = "Eroare la deschiderea conexiunii: " + ex.Message;
+                return;
+            }
+            OracleCommand cmd = new OracleCommand("genereaza_semnatura", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            try
+            {
+                cmd.ExecuteNonQuery();
+                Ecouu.Text = "Semnatura imaginii a fost generata cu succes pentru toate inregistrarile.";
+            }
+            catch (OracleException ex)
+            {
+                Ecouu.Text = "Eroare la executarea procedurii: " + ex.Message;
+            }
+            con.Close();
+        }
+
+        protected void regasireimagine_Click(object sender, EventArgs e)
+        {
+            Ecouu.Text = "";
+            if (imaginenucleara.HasFile)
+            {
+                imaginenucleara.SaveAs(@"C:\Users\Miruu\Desktop\proiect baze multi\imagini nucleare\" + imaginenucleara.FileName);
+
+                using (var img = System.IO.File.OpenRead(@"C:\Users\Miruu\Desktop\proiect baze multi\imagini nucleare\" + imaginenucleara.FileName))
+                {
+                    var imageBytes = new byte[img.Length];
+                    img.Read(imageBytes, 0, imageBytes.Length);
+
+                    try
+                    {
+                        con.Open();
+                    }
+                    catch (OracleException ex)
+                    {
+                        Ecouu.Text += "Eroare la deschiderea conexiunii: " + ex.Message;
+                        return;
+                    }
+
+                    OracleCommand cmd = new OracleCommand("regasireimagine", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.Add("fisier", OracleDbType.Blob).Value = imageBytes;
+                    cmd.Parameters.Add("regculoare", OracleDbType.Decimal).Value = Convert.ToDecimal(culoare.Text);
+                    cmd.Parameters.Add("regtextura", OracleDbType.Decimal).Value = Convert.ToDecimal(textura.Text);
+                    cmd.Parameters.Add("regforma", OracleDbType.Decimal).Value = Convert.ToDecimal(forma.Text);
+                    cmd.Parameters.Add("reglocatie", OracleDbType.Decimal).Value = Convert.ToDecimal(locatie.Text);
+
+                    cmd.Parameters.Add("idrez", OracleDbType.Int32).Direction = ParameterDirection.Output;
+
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (OracleException ex)
+                    {
+                        Ecouu.Text = "Eroare la execuția procedurii: " + ex.Message;
+                        con.Close();
+                        return;
+                    }
+
+                    idrezultat.Text = cmd.Parameters["idrez"].Value.ToString();
+                    idnuclear.Text = idrezultat.Text;
+
+                    con.Close();
+
+                 
+                    this.butonafisid_Click(this, e);
+                }
+            }
+            else
+            {
+                Ecouu.Text = "Fișier imagine lipsă!";
+            }
+        }
+
+        protected void butonafisid_Click(object sender, EventArgs e)
+        {
+            Ecouu.Text = "";
+
+            try
+            {
+                con.Open();
+
+               
+                OracleCommand cmd = new OracleCommand("SELECT imagine FROM scintigrafie WHERE id = :id", con);
+                cmd.Parameters.Add(":id", OracleDbType.Int32).Value = Convert.ToInt32(idrezultat.Text);
+
+                OracleDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+               
+                    byte[] imageBytes = (byte[])reader["imagine"];
+
+                    string base64String = Convert.ToBase64String(imageBytes);
+
+                    
+                    imagineaa.ImageUrl = "data:image/jpg;base64," + base64String;
+                }
+                else
+                {
+                    Ecouu.Text = "Nu s-a găsit imaginea cu acest ID.";
+                }
+
+                reader.Close();
+                con.Close();
+            }
+            catch (OracleException ex)
+            {
+                Ecouu.Text = "Eroare la obținerea imaginii din baza de date: " + ex.Message;
+            }
+            catch (Exception ex)
+            {
+                Ecouu.Text = "Eroare generală: " + ex.Message;
+            }
+        }
+
+
+    }
+}
+
+
+
